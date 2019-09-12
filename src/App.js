b129ac20eaa7d70a1454cd9fc78e56aab9b6e932
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2019-09-12 19:39:27 +0100

    added translation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import translate from 'translate';
 
 const App = () => {
 
 const [text, setText] = useState('')
+const [translateVal, setTranslate] = useState('')
+
+const getTranslation = async () => {
+const translation = await translate(text,{ to:'es', engine: 'yandex', key: process.env.REACT_APP_YANDEX_KEY})
+setTranslate(translation) 
+}
+
 
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <input value={text} onChange={(event) => setText(event.target.value)}/>
-        <button onClick={() => console.log(text)}>translate</button>
+        <button onClick={getTranslation}>translate</button>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        {translateVal}
         </p>
       </div>
     );
