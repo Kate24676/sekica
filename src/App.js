d210b213de02a86c3d397afb58fdbd4c0e0b1b3f
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2019-11-01 20:59:53 +0000

    changes made

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const [text, setText] = useState('')
 const [translateVal, setTranslate] = useState('')
 
 const getTranslation = async () => {
-  const translation = await translate(text,{ to:'br', engine: 'yandex', key: process.env.REACT_APP_YANDEX_KEY})
+  const translation = await translate(text,{ to:'hr', engine: 'yandex', key: process.env.REACT_APP_YANDEX_KEY})
   setTranslate(translation) 
 }
 
